78de5e85bacdbb5b715eb07c225055008fdedc36
Subhnkar <rajasubhankar2000@gmail.com>
2021-08-14 21:30:57 +0530

    ht

---
+++ javascript.js
@@ -0,0 +1,20 @@
+$(function(){
+    $("#ddd").click(function(){
+        
+        $("a").css("visibility","visible")
+        {
+            $("a").toggle()
+        }      
+    });
+    $("#dd").click(function(){
+        
+        $("button").css("visibility","visible")
+        {
+            $("button").toggle()
+        }
+        
+    });
+    
+   
+
+});
